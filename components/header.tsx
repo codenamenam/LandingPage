69e78ac846fb8fb67d5a83cdf952fce2327363f2
codenamenam <bluetire27@gmail.com>
2023-08-22 19:05:21 +0900

    Set header

---
+++ components/header.tsx
@@ -0,0 +1,122 @@
+import {
+  createStyles,
+  Menu,
+  Center,
+  Header,
+  Container,
+  Group,
+  Button,
+  Burger,
+  rem,
+  Text,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+
+const HEADER_HEIGHT = rem(60);
+
+const useStyles = createStyles((theme) => ({
+  inner: {
+    height: HEADER_HEIGHT,
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+
+  links: {
+    [theme.fn.smallerThan("sm")]: {
+      display: "none",
+    },
+  },
+
+  burger: {
+    [theme.fn.largerThan("sm")]: {
+      display: "none",
+    },
+  },
+
+  blurredHeader: {
+    backdropFilter: "blur(10px)", // 원하는 블러 정도로 조절
+    backgroundColor: "rgba(255, 255, 255, 0.1)", // 희망하는 배경색 및 투명도 조절
+  },
+
+  link: {
+    display: "block",
+    lineHeight: 1,
+    padding: `${rem(8)} ${rem(12)}`,
+    borderRadius: theme.radius.sm,
+    textDecoration: "none",
+    color:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[0]
+        : theme.colors.gray[7],
+    fontSize: theme.fontSizes.sm,
+    fontWeight: 500,
+
+    "&:hover": {
+      backgroundColor:
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[6]
+          : theme.colors.gray[0],
+    },
+  },
+
+  linkLabel: {
+    marginRight: rem(5),
+  },
+}));
+
+interface HeaderActionProps {
+  links: {
+    link: string;
+    label: string;
+  }[];
+}
+
+export function HeaderAction({ links }: HeaderActionProps) {
+  const { classes } = useStyles();
+  const [opened, { toggle }] = useDisclosure(false);
+
+  const items = links.map((link) => {
+    return (
+      <a
+        key={link.label}
+        href={link.link}
+        className={classes.link}
+        onClick={(event) => event.preventDefault()}
+      >
+        {link.label}
+      </a>
+    );
+  });
+
+  return (
+    <Header
+      className={classes.blurredHeader}
+      height={HEADER_HEIGHT}
+      sx={{
+        borderBottom: 0,
+        backgroundColor: "transparent",
+      }}
+      mb={120}
+      style={{ position: "fixed" }}
+    >
+      <Container className={classes.inner} fluid>
+        <Group>
+          <Burger
+            opened={opened}
+            onClick={toggle}
+            className={classes.burger}
+            size="sm"
+          />
+          <Text>Dopamine Defense</Text>
+        </Group>
+        <Group spacing={5} className={classes.links}>
+          {items}
+        </Group>
+        <Button radius="xl" h={30}>
+          Get early access
+        </Button>
+      </Container>
+    </Header>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,50 @@
+import { HeaderAction } from "@/components/header";
+import { Button, Center, Flex } from "@mantine/core";
+
+const links = [
+  {
+    link: "/about",
+    label: "Features",
+  },
+  {
+    link: "#1",
+    label: "Learn",
+  },
+  {
+    link: "/about",
+    label: "About",
+  },
+  {
+    link: "/pricing",
+    label: "Pricing",
+  },
+  {
+    link: "#2",
+    label: "Support",
+  },
+];
+
 export default function Home() {
-  return <>아</>;
+  return (
+    <div style={{ backgroundColor: "pink", margin: "0px" }}>
+      <HeaderAction links={links} />
+      <Center style={{ paddingTop: "60px" }}>
+        <Flex direction={"column"}>
+          <h1>아</h1>
+          <h1>아</h1>
+          <h1>아</h1>
+          <h1>아</h1>
+          <h1>아</h1>
+          <h1>아</h1>
+          <h1>아</h1>
+          <h1>아</h1>
+          <h1>아</h1>
+          <h1>아</h1>
+          <h1>아</h1>
+          <h1>아</h1>
+          <Button>ㅋㅋ</Button>
+        </Flex>
+      </Center>
+    </div>
+  );
 }
