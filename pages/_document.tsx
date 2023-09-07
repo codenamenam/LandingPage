3f8a5d6bd54cfef31e53755cf94fedf06ec1f81d
codenamenam <bluetire27@gmail.com>
2023-09-07 18:58:25 +0900

    channel talk

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,57 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <meta name="format-detection" content="telephone=no"></meta>
+        <Script
+          id="channelTalk"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `(function() {
+    var w = window;
+    if (w.ChannelIO) {
+      return (window.console.error || window.console.log || function(){})('ChannelIO script included twice.');
+    }
+    var ch = function() {
+      ch.c(arguments);
+    };
+    ch.q = [];
+    ch.c = function(args) {
+      ch.q.push(args);
+    };
+    w.ChannelIO = ch;
+    function l() {
+      if (w.ChannelIOInitialized) {
+        return;
+      }
+      w.ChannelIOInitialized = true;
+      var s = document.createElement('script');
+      s.type = 'text/javascript';
+      s.async = true;
+      s.src = 'https://cdn.channel.io/plugin/ch-plugin-web.js';
+      s.charset = 'UTF-8';
+      var x = document.getElementsByTagName('script')[0];
+      x.parentNode.insertBefore(s, x);
+    }
+    if (document.readyState === 'complete') {
+      l();
+    } else if (window.attachEvent) {
+      window.attachEvent('onload', l);
+    } else {
+      window.addEventListener('DOMContentLoaded', l, false);
+      window.addEventListener('load', l, false);
+    }
+  })();
+  ChannelIO('boot', {
+    "pluginKey": "cc8f51a2-62db-4a28-a5cb-bce2b87ebf3c"
+  });
+	`,
+          }}
+        />
+        ;
       </Head>
       <body style={{ margin: "0px" }}>
         <Main />
