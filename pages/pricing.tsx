8caa4759b0085804be1ef5878eb66bfad03f2e62
codenamenam <bluetire27@gmail.com>
2023-09-07 18:38:13 +0900

    Pricing

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -1,88 +1,213 @@
-import { SimpleGrid, Box, Text } from "@mantine/core";
+import {
+  SimpleGrid,
+  Title,
+  Box,
+  Text,
+  MediaQuery,
+  Center,
+  Flex,
+} from "@mantine/core";
+
+import { Fade } from "react-awesome-reveal";
 
 export default function Pricing() {
   return (
-    <SimpleGrid
-      cols={3}
-      breakpoints={[{ maxWidth: "xs", cols: 1 }]}
-      style={{ margin: "200px 100px 200px 100px" }}
-    >
-      <Box
-        sx={(theme) => ({
-          backgroundColor:
-            theme.colorScheme === "dark"
-              ? theme.colors.dark[6]
-              : theme.colors.gray[0],
-          textAlign: "center",
-          padding: theme.spacing.xl,
-          borderRadius: theme.radius.md,
-          cursor: "pointer",
+    <>
+      <Fade>
+        <MediaQuery smallerThan={"md"} styles={{ display: "none" }}>
+          <Center styles={{ width: "1060px" }}>
+            <Flex direction={"column"} style={{ margin: "200px 0 200px 0" }}>
+              <Center>
+                <Title size={50}>가격 안내</Title>
+              </Center>
+              <Flex direction={"row"} style={{ marginTop: "100px" }}>
+                <Box
+                  sx={(theme) => ({
+                    backgroundColor:
+                      theme.colorScheme === "dark"
+                        ? theme.colors.dark[6]
+                        : theme.colors.gray[1],
+                    textAlign: "center",
+                    padding: theme.spacing.xl,
+                    borderRadius: "20px",
+                    cursor: "pointer",
 
-          "&:hover": {
-            backgroundColor:
-              theme.colorScheme === "dark"
-                ? theme.colors.dark[5]
-                : theme.colors.gray[1],
-          },
-        })}
-      >
-        <Text style={{ fontSize: 24 }}>1개월</Text>
-        <Text style={{ fontSize: 18, fontWeight: "", color: "gray" }}>
-          <br />
-        </Text>
-        <Text style={{ fontSize: 30, fontWeight: "bold" }}>3000원</Text>
-      </Box>
-      <Box
-        sx={(theme) => ({
-          backgroundColor:
-            theme.colorScheme === "dark"
-              ? theme.colors.dark[6]
-              : theme.colors.gray[0],
-          textAlign: "center",
-          padding: theme.spacing.xl,
-          borderRadius: theme.radius.md,
-          cursor: "pointer",
+                    "&:hover": {
+                      backgroundColor:
+                        theme.colorScheme === "dark"
+                          ? theme.colors.dark[5]
+                          : theme.colors.gray[2],
+                    },
+                  })}
+                  style={{ width: "300px", margin: "5px" }}
+                >
+                  <Text style={{ fontSize: 24 }}>1개월</Text>
+                  <Text style={{ fontSize: 18, fontWeight: "", color: "gray" }}>
+                    <br />
+                  </Text>
+                  <Text style={{ fontSize: 30, fontWeight: "bold" }}>
+                    3000원
+                  </Text>
+                </Box>
+                <Box
+                  sx={(theme) => ({
+                    backgroundColor:
+                      theme.colorScheme === "dark"
+                        ? theme.colors.dark[6]
+                        : theme.colors.gray[1],
+                    textAlign: "center",
+                    padding: theme.spacing.xl,
+                    borderRadius: "20px",
+                    cursor: "pointer",
 
-          "&:hover": {
-            backgroundColor:
-              theme.colorScheme === "dark"
-                ? theme.colors.dark[5]
-                : theme.colors.gray[1],
-          },
-        })}
-      >
-        <Text style={{ fontSize: 24 }}>3개월</Text>
-        <Text style={{ fontSize: 18, fontWeight: "", color: "gray" }}>
-          {" "}
-          9̶0̶0̶0̶원
-        </Text>
-        <Text style={{ fontSize: 30, fontWeight: "bold" }}>8000원</Text>
-      </Box>
-      <Box
-        sx={(theme) => ({
-          backgroundColor:
-            theme.colorScheme === "dark"
-              ? theme.colors.dark[6]
-              : theme.colors.gray[0],
-          textAlign: "center",
-          padding: theme.spacing.xl,
-          borderRadius: theme.radius.md,
-          cursor: "pointer",
+                    "&:hover": {
+                      backgroundColor:
+                        theme.colorScheme === "dark"
+                          ? theme.colors.dark[5]
+                          : theme.colors.gray[2],
+                    },
+                  })}
+                  style={{ width: "300px", margin: "5px" }}
+                >
+                  <Text style={{ fontSize: 24 }}>3개월</Text>
+                  <Text style={{ fontSize: 18, fontWeight: "", color: "gray" }}>
+                    {" "}
+                    9̶0̶0̶0̶원
+                  </Text>
+                  <Text style={{ fontSize: 30, fontWeight: "bold" }}>
+                    8000원
+                  </Text>
+                </Box>
+                <Box
+                  sx={(theme) => ({
+                    backgroundColor:
+                      theme.colorScheme === "dark"
+                        ? theme.colors.dark[6]
+                        : theme.colors.gray[1],
+                    textAlign: "center",
+                    padding: theme.spacing.xl,
+                    borderRadius: "20px",
+                    cursor: "pointer",
 
-          "&:hover": {
-            backgroundColor:
-              theme.colorScheme === "dark"
-                ? theme.colors.dark[5]
-                : theme.colors.gray[1],
-          },
-        })}
-      >
-        <Text style={{ fontSize: 24 }}>6개월</Text>
-        <Text style={{ fontSize: 18, fontWeight: "", color: "gray" }}>
-          1̶8̶0̶0̶0̶원
-        </Text>
-        <Text style={{ fontSize: 30, fontWeight: "bold" }}>15000원</Text>
-      </Box>
-    </SimpleGrid>
+                    "&:hover": {
+                      backgroundColor:
+                        theme.colorScheme === "dark"
+                          ? theme.colors.dark[5]
+                          : theme.colors.gray[2],
+                    },
+                  })}
+                  style={{ width: "300px", margin: "5px" }}
+                >
+                  <Text style={{ fontSize: 24 }}>6개월</Text>
+                  <Text style={{ fontSize: 18, fontWeight: "", color: "gray" }}>
+                    1̶8̶0̶0̶0̶원
+                  </Text>
+                  <Text style={{ fontSize: 30, fontWeight: "bold" }}>
+                    15000원
+                  </Text>
+                </Box>
+              </Flex>
+            </Flex>
+          </Center>
+        </MediaQuery>
+
+        <MediaQuery largerThan={"md"} styles={{ display: "none" }}>
+          <Center styles={{ width: "350px" }}>
+            <Flex direction={"column"} style={{ margin: "150px 0 150px 0" }}>
+              <Center>
+                <Title size={40}>가격 안내</Title>
+              </Center>
+              <Flex direction={"column"} style={{ marginTop: "50px" }}>
+                <Box
+                  sx={(theme) => ({
+                    backgroundColor:
+                      theme.colorScheme === "dark"
+                        ? theme.colors.dark[6]
+                        : theme.colors.gray[1],
+                    textAlign: "center",
+                    padding: theme.spacing.xl,
+                    borderRadius: "20px",
+                    cursor: "pointer",
+
+                    "&:hover": {
+                      backgroundColor:
+                        theme.colorScheme === "dark"
+                          ? theme.colors.dark[5]
+                          : theme.colors.gray[2],
+                    },
+                  })}
+                  style={{ width: "300px", margin: "5px" }}
+                >
+                  <Text style={{ fontSize: 24 }}>1개월</Text>
+                  <Text style={{ fontSize: 18, fontWeight: "", color: "gray" }}>
+                    <br />
+                  </Text>
+                  <Text style={{ fontSize: 30, fontWeight: "bold" }}>
+                    3000원
+                  </Text>
+                </Box>
+                <Box
+                  sx={(theme) => ({
+                    backgroundColor:
+                      theme.colorScheme === "dark"
+                        ? theme.colors.dark[6]
+                        : theme.colors.gray[1],
+                    textAlign: "center",
+                    padding: theme.spacing.xl,
+                    borderRadius: "20px",
+                    cursor: "pointer",
+
+                    "&:hover": {
+                      backgroundColor:
+                        theme.colorScheme === "dark"
+                          ? theme.colors.dark[5]
+                          : theme.colors.gray[2],
+                    },
+                  })}
+                  style={{ width: "300px", margin: "5px" }}
+                >
+                  <Text style={{ fontSize: 24 }}>3개월</Text>
+                  <Text style={{ fontSize: 18, fontWeight: "", color: "gray" }}>
+                    {" "}
+                    9̶0̶0̶0̶원
+                  </Text>
+                  <Text style={{ fontSize: 30, fontWeight: "bold" }}>
+                    8000원
+                  </Text>
+                </Box>
+                <Box
+                  sx={(theme) => ({
+                    backgroundColor:
+                      theme.colorScheme === "dark"
+                        ? theme.colors.dark[6]
+                        : theme.colors.gray[1],
+                    textAlign: "center",
+                    padding: theme.spacing.xl,
+                    borderRadius: "20px",
+                    cursor: "pointer",
+
+                    "&:hover": {
+                      backgroundColor:
+                        theme.colorScheme === "dark"
+                          ? theme.colors.dark[5]
+                          : theme.colors.gray[2],
+                    },
+                  })}
+                  style={{ width: "300px", margin: "5px" }}
+                >
+                  <Text style={{ fontSize: 24 }}>6개월</Text>
+                  <Text style={{ fontSize: 18, fontWeight: "", color: "gray" }}>
+                    1̶8̶0̶0̶0̶원
+                  </Text>
+                  <Text style={{ fontSize: 30, fontWeight: "bold" }}>
+                    15000원
+                  </Text>
+                </Box>
+              </Flex>
+            </Flex>
+          </Center>
+        </MediaQuery>
+      </Fade>
+    </>
   );
 }
