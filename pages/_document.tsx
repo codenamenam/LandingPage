72bceb4f7cebb675e523f685836182d8fdc380cf
codenamenam <bluetire27@gmail.com>
2023-09-07 13:23:57 +0900

    meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="format-detection" content="telephone=no"></meta>
+      </Head>
       <body style={{ margin: "0px" }}>
         <Main />
         <NextScript />
