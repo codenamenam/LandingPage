563c1ee521b7727e22e92af54d5b33bd40fe0b6e
codenamenam <bluetire27@gmail.com>
2023-09-12 13:19:34 +0900

    Fix GA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,23 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
+import * as gtag from "../lib/gtag";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Document() {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on("hashChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+      router.events.off("hashChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <Html lang="en">
       <Head>
@@ -16,8 +32,9 @@ export default function Document() {
           strategy="lazyOnload"
           dangerouslySetInnerHTML={{
             __html: `
-window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
+            window.dataLayer = window.dataLayer || [];
+  function gtag(){
+    dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-YDM6TNMLS9');
